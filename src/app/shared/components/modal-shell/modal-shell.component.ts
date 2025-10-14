0f9f67c83ca3e5bc98c601d1e5f8ac08069466c3
Ayesha Javaid <ayeshajavaid5005@gmail.com>
2025-10-14 09:32:25 -0700

    Teacher assignments components managed, reactive form and modal implemented + student's assignments managed + modal shell created

--- src/app/shared/components/modal-shell/modal-shell.component.ts
+++ src/app/shared/components/modal-shell/modal-shell.component.ts
@@ -1,18 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-modal-shell',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './modal-shell.component.html',
   styleUrl: './modal-shell.component.css',
 })
 export class ModalShellComponent {
   @Input() show = false;
   @Input() title = '';
+  @Input() actionLabel = 'Submit';
+  @Input() isFormValid = true; // ✅ NEW: controls whether button is disabled
+
   @Output() close = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<void>();
 
   onOverlayClick(event: MouseEvent) {
-    this.close.emit();
+    if (event.target === event.currentTarget) {
+      this.close.emit();
+    }
   }
 }
